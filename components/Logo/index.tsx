90165eaf2d89af65c2cf46ca794645bcc9658922
Gencko94 <ahmad94.dev@gmail.com>
2021-09-04 13:12:41 +0300

    add .htaccess

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -2,8 +2,7 @@ import styled from "styled-components";
 import logo from "../../public/newLogo.png";
 
 const Logo = styled.img.attrs(() => ({
-  src: "/newLogo.png",
+  src: "newLogo.png",
   alt: "Sweetat logo",
-  layout: "fill",
 }))``;
 export default Logo;
