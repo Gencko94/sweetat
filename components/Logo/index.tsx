fe5c8dc7b655434fa78034d2e8bb8b6ca8c6365d
Gencko94 <ahmad94.dev@gmail.com>
2021-09-04 13:21:29 +0300

    fix logo url , add trailing slash to next.config

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import logo from "../../public/newLogo.png";
 
 const Logo = styled.img.attrs(() => ({
-  src: "newLogo.png",
+  src: "/newlogo.png",
   alt: "Sweetat logo",
 }))``;
 export default Logo;
